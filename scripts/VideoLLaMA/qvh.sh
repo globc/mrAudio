5b046ea6fdad7768dc6c6bf3c1463a6e13686e97
globc <globcxy@gmail.com>
2024-11-13 11:43:00 +0100

    added data_preprocess

--- scripts/VideoLLaMA/qvh.sh
+++ scripts/VideoLLaMA/qvh.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python3 evaluate.py \
             --model videollama \
             --dataset QVH \
-            --video-folder /work/scratch/kurse/kurs00079/cb14syta/mr-Audio/mr_BLIP_data/QVH/videos \
-            --annotation-file /work/scratch/kurse/kurs00079/cb14syta/mr-Audio/mr_BLIP_data/QVH/lavis/val.json \
+            --video-folder /work/scratch/kurse/kurs00079/cb14syta/mr-Audio2/data/QVH/videos \
+            --annotation-file /work/scratch/kurse/kurs00079/cb14syta/mr-Audio2/data/QVH/lavis/val.json \
             --output-file /work/scratch/kurse/kurs00079/cb14syta/mr-Audio2/outputs/QVH/videollama/output.json \
