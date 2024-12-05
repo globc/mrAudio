2324980e10a3e93ddcc0b6250fd59d69daa34820
leonlolly <leon@wenderoth.de>
2024-12-05 16:59:39 +0100

    draft x

--- finetune.py
+++ finetune.py
@@ -1,96 +1,61 @@
-import logging
+import argparse
 import time
+import torch
+import wandb
+from torch._C.cpp.nn import Module
+from mrAudio.evaluate import MRDataset, collate_fn
+from torch.utils.data import  DataLoader
+import torch.optim as optim
+from mrAudio.utils.utils import prepare_sample
+from torch.optim.lr_scheduler import LRScheduler
+import torch.distributed as dist
 
-
-def train(self):
+def train(model,start_epoch:int,max_epoch:int,valid_splits:[],val_freq:int,data_loader:DataLoader,evaluate_only:bool=False):
     start_time = time.time()
     best_agg_metric = 0
     best_epoch = 0
 
-    self.log_config()
+    """
+    learning_rate = 0.001  # Adjust as needed
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    """
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    lr_scheduler = LRScheduler(optimizer=optimizer,)
 
     # resume from checkpoint if specified
-    if not self.evaluate_only and self.resume_ckpt_path is not None:
-        self._load_checkpoint(self.resume_ckpt_path)
+    ##if not self.evaluate_only and self.resume_ckpt_path is not None:
+    ##    self._load_checkpoint(self.resume_ckpt_path)
 
-    for cur_epoch in range(self.start_epoch, self.max_epoch):
-        # training phase
-        if not self.evaluate_only:
-            logging.info("Start training")
-            train_stats = self.train_epoch(cur_epoch)
-            self.log_stats(split_name="train", stats=train_stats)
+    for cur_epoch in range(start_epoch, max_epoch):
+        if len(valid_splits) > 0 and (evaluate_only or cur_epoch%val_freq == 0):
+            for split_name in valid_splits:
+                train_epoch(epoch=cur_epoch,model=model,iters_per_epoch=int(len(valid_splits)/val_freq),data_loader=data_loader,lr_scheduler=lr_scheduler,optimizer=optimizer)
 
-        # evaluation phase
-        if len(self.valid_splits) > 0:
-            for split_name in self.valid_splits:
-                logging.info("Evaluating on {}.".format(split_name))
 
-                val_log = self.eval_epoch(
-                    split_name=split_name, cur_epoch=cur_epoch
-                )
-                if val_log is not None:
-                    if is_main_process():
-                        assert (
-                            "agg_metrics" in val_log
-                        ), "No agg_metrics found in validation log."
-
-                        agg_metrics = val_log["agg_metrics"]
-                        if agg_metrics > best_agg_metric and split_name == "val":
-                            best_epoch, best_agg_metric = cur_epoch, agg_metrics
-
-                            self._save_checkpoint(cur_epoch, is_best=True)
-
-                        val_log.update({"best_epoch": best_epoch})
-                        self.log_stats(val_log, split_name)
-
-def train_epoch(self,
+def train_epoch(
         epoch,
         iters_per_epoch,
         model,
         data_loader,
-        optimizer,
         lr_scheduler,
+        optimizer,
         scaler=None,
         start_iters=None,
         log_freq=50,
         cuda_enabled=False,
         accum_grad_iters=1):
     
-    self.model.train()
+    model.train()  ## missing
 
-
-
-    use_amp = scaler is not None
-
-    if not hasattr(data_loader, "__next__"):
-        # convert to iterator if not already
-        data_loader = iter(data_loader)
-
-    metric_logger = MetricLogger(delimiter="  ")
-    metric_logger.add_meter("lr", SmoothedValue(window_size=1, fmt="{value:.6f}"))
-    metric_logger.add_meter("loss", SmoothedValue(window_size=1, fmt="{value:.4f}"))
-
-    # if iter-based runner, schedule lr based on inner epoch.
-    logging.info(
-        "Start training epoch {}, {} iters per inner epoch.".format(
-            epoch, iters_per_epoch
-        )
-    )
-    header = "Train: data epoch: [{}]".format(epoch)
-    if start_iters is None:
-        # epoch-based runner
-        inner_epoch = epoch
-    else:
-        # In iter-based runner, we schedule the learning rate based on iterations.
-        inner_epoch = start_iters // iters_per_epoch
-        header = header + "; inner epoch [{}]".format(inner_epoch)
-
-    for i in metric_logger.log_every(range(iters_per_epoch), log_freq, header):
-        # if using iter-based runner, we stop after iters_per_epoch iterations.
-        if i >= iters_per_epoch:
-            break
-
+    losses = []
+    for i in range(iters_per_epoch):
         samples = next(data_loader)
+        if start_iters is None:
+            inner_epoch = epoch
+        else:
+
+            inner_epoch = start_iters // iters_per_epoch
 
         samples = prepare_sample(samples, cuda_enabled=cuda_enabled)
         samples.update(
@@ -101,46 +66,72 @@ def train_epoch(self,
             }
         )
 
+
+
         lr_scheduler.step(cur_epoch=inner_epoch, cur_step=i)
 
-        with torch.cuda.amp.autocast(enabled=use_amp):
-            loss = self.train_step(model=model, samples=samples)
+        with torch.cuda.amp.autocast(enabled=True):
+            loss = model.forward(**samples)
+            losses.append(loss)
 
-        # after_train_step()
-        if use_amp:
-            scaler.scale(loss).backward()
-        else:
-            loss.backward()
+        scaler.scale(loss).backward()
 
-        # update gradients every accum_grad_iters iterations
+
         if (i + 1) % accum_grad_iters == 0:
-            if use_amp:
-                scaler.step(optimizer)
-                scaler.update()
-            else:
-                optimizer.step()
+            scaler.step(optimizer)
+            scaler.update()
             optimizer.zero_grad()
 
-        metric_logger.update(loss=loss.item())
-        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
         # log to wandb
-        if is_main_process() and wandb.run is not None:
+        if dist.get_rank() == 0 and wandb.run is not None:
             wandb.log(
                 {
                     "train/lr": optimizer.param_groups[0]["lr"],
                 }
             )
+    return losses
 
-        # print("breaking in train_inner_loop in moment_retrieval.py")
-        # break
+def run_train(args):
 
-    # after train_epoch()
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    logging.info("Averaged stats: " + str(metric_logger.global_avg()))
+    model:Module
+    if args.model == "X-InstructBLIP":
+        from models.xinstructblip import XInstructBLIP
+        model = XInstructBLIP(args.model_path, args.audio_encoder)
 
-    return {
-        k: "{:.3f}".format(meter.global_avg)
-        for k, meter in metric_logger.meters.items()
-}
+    if args.model == "VideoLLaMA":
+        from models.videollama import VideoLLaMA
+        model = VideoLLaMA(args.model_path)
+
+    dataset:MRDataset
+    if args.dataset in ["QVH", "Charades_STA"]:
+        dataset = MRDataset(vis_root=args.video_folder, ann_path=args.annotation_file)
+
+    dataloader = DataLoader(dataset=dataset,
+                            shuffle=False,
+                            batch_size=args.batch_size,
+                            num_workers=args.num_workers,
+                            collate_fn=collate_fn)
+
+
+    train(model,0,4,0,1,dataloader)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model', help='', required=True)
+    parser.add_argument('--model-path', help='', required=True)
+    parser.add_argument('--audio-encoder', help='', required=False)
+    parser.add_argument('--video-folder', help='Directory containing video files.', required=True)
+    parser.add_argument('--annotation-file', help='Path to the ground truth file containing question.', required=True)
+    parser.add_argument('--output-file', help='Directory to save the model results JSON.', required=True)
+    parser.add_argument("--num-chunks", type=int, default=1)
+    parser.add_argument("--chunk-idx", type=int, default=0)
+    parser.add_argument("--device", type=str, required=False, default='cuda:0')
+    parser.add_argument("--batch-size", type=int, required=False, default=1)
+    parser.add_argument("--num-workers", type=int, required=False, default=8)
+    parser.add_argument("--dataset", type=str, required=True)
+    args = parser.parse_args()
+
+    run_train(args)
