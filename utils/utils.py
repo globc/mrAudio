14a326abf70156a93db3d110535baa99c3e50f24
leonlolly <leon@wenderoth.de>
2024-12-05 16:58:29 +0100

    add cuda loading for later

--- utils/utils.py
+++ utils/utils.py
@@ -11,6 +11,40 @@ import wandb
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 
+def apply_to_sample(f, sample):
+    ## add check for datasets that return none samples for missing items
+    if sample == None or len(sample) == 0:
+        return {}
+
+    def _apply(x):
+        if torch.is_tensor(x):
+            return f(x)
+        elif isinstance(x, dict):
+            return {key: _apply(value) for key, value in x.items()}
+        elif isinstance(x, list):
+            return [_apply(x) for x in x]
+        else:
+            return x
+
+    return _apply(sample)
+
+
+def move_to_cuda(sample):
+    def _move_to_cuda(tensor):
+        return tensor.cuda()
+
+    return apply_to_sample(_move_to_cuda, sample)
+
+
+def prepare_sample(samples, cuda_enabled=True):
+    if cuda_enabled:
+        samples = move_to_cuda(samples)
+
+    # TODO fp16 support
+
+    return samples
+
+
 def convert_percentages_to_second(percentages:str, duration:int):
     if not re.match(r"\[\[.*\]\]", percentages):
         return "[[-1, -1]]"
