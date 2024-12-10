b451145e57f6adb80441193096a7d0a107cb69f3
leonlolly <leon@wenderoth.de>
2024-12-10 14:01:07 +0100

    fix $CUDA_VISIBLE_DEVICES

--- scripts/train/X-InstructBLIP/qvh.sh
+++ scripts/train/X-InstructBLIP/qvh.sh
@@ -1,4 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 finetune.py \
+export CUDA_VISIBLE_DEVICES=nvidia-smi --list-gpus | grep GPU | awk -F: '{print $1}' | awk '{print $2}' | tr '\n' ',' | sed 's/,$//'
+echo $CUDA_VISIBLE_DEVICES
+
+python -m torch.distributed.run --nproc_per_node=4 finetune.py \
             --model X-InstructBLIP \
             --model-path $HPC_SCRATCH/mrAudio/checkpoints/X-InstructBLIP/vicuna-7b-v1.1 \
             --audio-encoder $HPC_SCRATCH/mrAudio/checkpoints/X-InstructBLIP/BEATs_iter3_plus_AS2M.pt \
